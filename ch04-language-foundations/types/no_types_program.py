ac53fc13c7f11d21c6b3b70c3059cfbf9e105a35
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-02 13:51:43 -0800

    With type hints things are safer!

---
+++ ch04-language-foundations/types/no_types_program.py
@@ -0,0 +1,37 @@
+from collections import namedtuple
+
+Item = namedtuple("Item", "name, value")
+
+running_max = None
+
+
+def counter(items):
+    global running_max
+    total = 0
+
+    for i in items:
+        total += i.value
+
+    if not running_max or total > running_max:
+        running_max = total
+
+    return total
+
+
+def main():
+    print("Let's create some items")
+
+    dinner_items = [Item('Pizza', 20), Item('Beer', 9), Item('Beer', 9)]
+    breakfast_items = [Item('Pancakes', 11), Item('Bacon', 4), Item('Coffee', 3), Item('Coffee', 3), Item('Scone', 2)]
+
+    dinner_total = counter(dinner_items)
+    print(f"Dinner was ${dinner_total:,.02f}")
+
+    breakfast_total = counter(breakfast_items)
+    print(f"Breakfast was ${breakfast_total:,.02f}")
+
+    print(f"Today your most expensive meal costs ${running_max:.02f}")
+
+
+if __name__ == '__main__':
+    main()
