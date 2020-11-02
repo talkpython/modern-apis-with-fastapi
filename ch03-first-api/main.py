b51256f2e9bd7eb2d3e7bcbdcecb26cd86b3d736
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-02 11:28:04 -0800

    Fist API done.

--- ch03-first-api/main.py
+++ ch03-first-api/main.py
@@ -9,9 +9,9 @@ api = fastapi.FastAPI()
 @api.get('/api/calculate')
 def calculate(x: int, y: int, z: Optional[int] = None):
     if z == 0:
-        return fastapi.Response(content='{"error": "ERROR: Z cannot be zero."}',
-                                media_type="application/json",
-                                status_code=400)
+        return fastapi.responses.JSONResponse(
+            content={"error": "ERROR: Z cannot be zero."},
+            status_code=400)
 
     value = x + y
 
@@ -24,5 +24,6 @@ def calculate(x: int, y: int, z: Optional[int] = None):
         'z': z,
         'value': value
     }
+
 
 uvicorn.run(api, port=8000, host="127.0.0.1")
