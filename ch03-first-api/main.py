e6d87b76be363471784aec43dce4ddcd2ee88c78
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-02 13:29:31 -0800

    Add something of a home page to avoid the 404 for /

--- ch03-first-api/main.py
+++ ch03-first-api/main.py
@@ -6,6 +6,20 @@ import uvicorn
 api = fastapi.FastAPI()
 
 
+@api.get('/')
+def index():
+    body = "<html>" \
+           "<body style='padding: 10px;'>" \
+           "<h1>Welcome to the API</h1>" \
+           "<div>" \
+           "Try it: <a href='/api/calculate?x=7&y=11'>/api/calculate?x=7&y=11</a>" \
+           "</div>" \
+           "</body>" \
+           "</html>"
+
+    return fastapi.responses.HTMLResponse(content=body)
+
+
 @api.get('/api/calculate')
 def calculate(x: int, y: int, z: Optional[int] = None):
     if z == 0:
