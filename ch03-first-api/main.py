5c5374a8c388c60275583a160b6450dad6976087
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-02 11:04:13 -0800

    Returning an error response, bad request.

--- ch03-first-api/main.py
+++ ch03-first-api/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import fastapi
 import uvicorn
 
@@ -5,12 +7,22 @@ api = fastapi.FastAPI()
 
 
 @api.get('/api/calculate')
-def calculate(x: int, y: int, z: int = 10):
-    value = (x + y)*z
+def calculate(x: int, y: int, z: Optional[int] = None):
+    if z == 0:
+        return fastapi.Response(content='{"error": "ERROR: Z cannot be zero."}',
+                                media_type="application/json",
+                                status_code=400)
+
+    value = x + y
+
+    if z is not None:
+        value /= z
 
     return {
+        'x': x,
+        'y': y,
+        'z': z,
         'value': value
     }
-
 
 uvicorn.run(api, port=8000, host="127.0.0.1")
