c13533f15ac42b7e365dee2b792222178014c4f2
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-02 10:34:58 -0800

    The most basic api running!

--- ch03-first-api/main.py
+++ ch03-first-api/main.py
@@ -1,3 +1,16 @@
 import fastapi
+import uvicorn
 
-print("hello fast api!")
+api = fastapi.FastAPI()
+
+
+@api.get('/api/calculate')
+def calculate():
+    value = 2 + 2
+
+    return {
+        'value': value
+    }
+
+
+uvicorn.run(api, port=8000, host="127.0.0.1")
