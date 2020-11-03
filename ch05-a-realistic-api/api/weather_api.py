c1781535e1ee1dc0f1db4c9cd1641907eb358a8c
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-03 14:51:15 -0800

    API Signature with bare parameters.

--- ch05-a-realistic-api/api/weather_api.py
+++ ch05-a-realistic-api/api/weather_api.py
@@ -1,8 +1,14 @@
+from typing import Optional
+
 import fastapi
 
 router = fastapi.APIRouter()
 
 
-@router.get('/api/weather')
-def weather():
-    return "Some report"
+@router.get('/api/weather/{city}')
+def weather(city: str,
+            state: Optional[str] = None,
+            country: str = 'US',
+            units: Optional[str] = 'metric'):
+
+    return f"{city}, {state}, {country} in {units}"
