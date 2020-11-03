e78a5d400eb97b5970f50062bd372801197c6cf6
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-03 14:55:39 -0800

    Binding pydantic models to route params.

--- ch05-a-realistic-api/api/weather_api.py
+++ ch05-a-realistic-api/api/weather_api.py
@@ -1,14 +1,13 @@
 from typing import Optional
 
 import fastapi
+from fastapi import Depends
+
+from models.location import Location
 
 router = fastapi.APIRouter()
 
 
 @router.get('/api/weather/{city}')
-def weather(city: str,
-            state: Optional[str] = None,
-            country: str = 'US',
-            units: Optional[str] = 'metric'):
-
-    return f"{city}, {state}, {country} in {units}"
+def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+    return f"{loc.city}, {loc.state}, {loc.country} in {units}"
