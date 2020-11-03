4af2194d8bdb733d1c2cbe60b201a9c04bdf7267
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-03 15:21:42 -0800

    Now calling the API with requests.

--- ch05-a-realistic-api/services/openweather_service.py
+++ ch05-a-realistic-api/services/openweather_service.py
@@ -1,8 +1,20 @@
 from typing import Optional
+import requests
 
 api_key: Optional[str] = None
 
 
 def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
-    q = f'{city},{country}'
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}?units={units}'
+    if state:
+        q = f'{city},{state},{country}'
+    else:
+        q = f'{city},{country}'
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}'
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    data = resp.json()
+    forecast = data['main']
+    return forecast
