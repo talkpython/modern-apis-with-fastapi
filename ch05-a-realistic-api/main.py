e523a5aa446af904a3a8471ae186c991aad0c739
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-03 11:00:30 -0800

    Now with jinja templates

--- ch05-a-realistic-api/main.py
+++ ch05-a-realistic-api/main.py
@@ -1,12 +1,15 @@
 import fastapi
 import uvicorn
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
 
 api = fastapi.FastAPI()
+templates = Jinja2Templates('templates')
 
 
 @api.get('/')
-def index():
-    return "Hello Weather app!"
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
 
 
 if __name__ == '__main__':
