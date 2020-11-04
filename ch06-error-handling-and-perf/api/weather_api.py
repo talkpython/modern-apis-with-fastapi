b6cc85b364dcaa8cab7731d6019f04edfcf34185
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-03 16:04:12 -0800

    With proper validation responses.

--- ch06-error-handling-and-perf/api/weather_api.py
+++ ch06-error-handling-and-perf/api/weather_api.py
@@ -4,6 +4,7 @@ import fastapi
 from fastapi import Depends
 
 from models.location import Location
+from models.validation_error import ValidationError
 from services import openweather_service
 
 router = fastapi.APIRouter()
@@ -11,6 +12,8 @@ router = fastapi.APIRouter()
 
 @router.get('/api/weather/{city}')
 async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
-    report = await openweather_service.get_report_async(loc.city, loc.state, loc.country, units)
 
-    return report
+    try:
+        return await openweather_service.get_report_async(loc.city, loc.state, loc.country, units)
+    except ValidationError as ve:
+        return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
