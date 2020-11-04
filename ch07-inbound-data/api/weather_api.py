c11bc1cb6955a626c09362054b30eee12e6ce179
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-04 08:47:58 -0800

    Returning some weather event reports.

--- ch07-inbound-data/api/weather_api.py
+++ ch07-inbound-data/api/weather_api.py
@@ -1,21 +1,28 @@
-from typing import Optional
+from typing import Optional, List
 
 import fastapi
 from fastapi import Depends
 
 from models.location import Location
+from models.reports import Report
 from models.validation_error import ValidationError
-from services import openweather_service
+from services import openweather_service, report_service
 
 router = fastapi.APIRouter()
 
 
 @router.get('/api/weather/{city}')
 async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
-
     try:
         return await openweather_service.get_report_async(loc.city, loc.state, loc.country, units)
     except ValidationError as ve:
         return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
     except Exception as x:
         return fastapi.Response(content=str(x), status_code=500)
+
+
+@router.get('/api/reports', name='all_reports')
+async def reports_get() -> List[Report]:
+    await report_service.add_report("A", Location(city="Portland"))
+    await report_service.add_report("B", Location(city="NYC"))
+    return await report_service.get_reports()
