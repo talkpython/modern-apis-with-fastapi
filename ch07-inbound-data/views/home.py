03f419bcf08369cc0f9cdecae27bee58d6c4bebd
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-04 09:22:00 -0800

    Showing recent weather events on the home page.

--- ch07-inbound-data/views/home.py
+++ ch07-inbound-data/views/home.py
@@ -2,13 +2,18 @@ import fastapi
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+from services import report_service
+
 templates = Jinja2Templates('templates')
 router = fastapi.APIRouter()
 
 
 @router.get('/')
-def index(request: Request):
-    return templates.TemplateResponse('home/index.html', {'request': request})
+async def index(request: Request):
+    events = await report_service.get_reports()
+    data = {'request': request, 'events': events}
+
+    return templates.TemplateResponse('home/index.html', data)
 
 
 @router.get('/favicon.ico')
