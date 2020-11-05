3c39c03e6f3110076cb6b6cdace9f14fda78a4b8
Michael Kennedy <mikeckennedy@gmail.com>
2020-11-04 16:04:39 -0800

    Add some server config files for presentation.

---
+++ ch08-deployment/server/scripts/server_setup.sh
@@ -0,0 +1,85 @@
+#!/usr/bin/env bash
+
+# Consider running these two commands separately
+# Do a reboot before continuing.
+apt update
+apt upgrade -y
+
+apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install some OS dependencies:
+sudo apt-get install -y -q build-essential git unzip zip nload tree
+sudo apt-get install -y -q python3-pip python3-dev python3-venv
+
+# Stop the hackers
+sudo apt install fail2ban -y
+
+ufw allow 22
+ufw allow 80
+ufw allow 443
+ufw enable
+
+# Basic git setup
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=720000'
+
+# Be sure to put your info here:
+git config --global user.email "you@email.com"
+git config --global user.name "Your name"
+
+apt install acl -y
+useradd -M apiuser
+usermod -L apiuser
+setfacl -m u:apiuser:rwx /apps/logs/weather_api
+
+
+# Web app file structure
+mkdir /apps
+chmod 777 /apps
+mkdir /apps/logs
+mkdir /apps/logs/weather_api
+mkdir /apps/logs/weather_api/app_log
+# chmod 777 /apps/logs/weather_api
+cd /apps
+
+# Create a virtual env for the app.
+cd /apps
+python3 -m venv venv
+source /apps/venv/bin/activate
+pip install --upgrade pip setuptools
+pip install --upgrade httpie glances
+pip install --upgrade gunicorn
+
+# clone the repo:
+cd /apps
+git clone https://github.com/talkpython/modern-apis-with-fastapi app_repo
+
+# Setup the web app:
+cd /apps/app_repo/ch08-deployment
+pip install -r requirements.txt
+
+# Copy and enable the daemon
+cp /apps/app_repo/ch08-deployment/server/units/weather.service /etc/systemd/system/
+
+systemctl start weather
+systemctl status weather
+systemctl enable weather
+
+# Setup the public facing server (NGINX)
+apt install nginx
+
+# CAREFUL HERE. If you are using default, maybe skip this
+rm /etc/nginx/sites-enabled/default
+
+cp /apps/app_repo/ch08-deployment/server/nginx/weather.nginx /etc/nginx/sites-enabled/
+update-rc.d nginx enable
+service nginx restart
+
+
+# Optionally add SSL support via Let's Encrypt:
+# https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-18-04
+
+add-apt-repository ppa:certbot/certbot
+apt install python-certbot-nginx
+certbot --nginx -d weatherapi.talkpython.com
