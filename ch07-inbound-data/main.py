7d9d41abd867caee10da158cb17ca05b0cd4e8a1
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-04 09:37:32 -0800

    Fixes #2. Runs the app under "python main.py" and "uvicorn main:api"

--- ch07-inbound-data/main.py
+++ ch07-inbound-data/main.py
@@ -41,9 +41,25 @@ def configure_fake_data():
     # This was added to make it easier to test the weather event reporting
     # We have /api/reports but until you submit new data each run, it's missing
     # So this will give us something to start from.
-    loc = Location(city="Portland", state="OR", country="US")
-    asyncio.run(report_service.add_report("Misty sunrise today, beautiful!", loc))
-    asyncio.run(report_service.add_report("Clouds over downtown.", loc))
+    loop = None
+    try:
+        loop = asyncio.get_running_loop()
+        print("got loop!", loop)
+    except RuntimeError:
+        pass  # Boo, why can't I just get nothing back?
+
+    if not loop:
+        loop = asyncio.get_event_loop()
+
+    try:
+        loc = Location(city="Portland", state="OR", country="US")
+        loop.run_until_complete(report_service.add_report("Misty sunrise today, beautiful!", loc))
+        loop.run_until_complete(report_service.add_report("Clouds over downtown.", loc))
+    except RuntimeError:
+        print("Note: Could not import starter date, this fails on some systems and "
+              "some ways of running the app under uvicorn.")
+        print("Fake starter data will no appear on home page.")
+        print("Once you add data with the client, it will appear properly.")
 
 
 if __name__ == '__main__':
